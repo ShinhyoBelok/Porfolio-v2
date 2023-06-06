67d38c09b101b2076233eb86276be7d896399b6d
ShinhyoBelok <shibo6937@gmail.com>
2023-06-06 17:18:23 -0400

    Add dynamic expertise section

---
+++ logic/about.js
@@ -0,0 +1,26 @@
+const sideArrows = document.querySelectorAll('.side_arrow');
+const aboutList = document.querySelectorAll('.dropdownHead');
+const ulsAbout = document.querySelectorAll('.ulAbout');
+
+function rotateImage(index) {
+  const arrow = sideArrows[index];
+  arrow.classList.toggle("rotate");
+}
+
+function removeBorder(index) {
+  const aboutItem = aboutList[index];
+  aboutItem.classList.toggle("open");
+}
+
+function fillContent(index) {
+  const aboutUl = ulsAbout[index];
+  aboutUl.classList.toggle("showUlAbout");
+}
+
+aboutList.forEach((element, index) => {
+  element.addEventListener('click', (event) => {
+    rotateImage(index);
+    removeBorder(index);
+    fillContent(index);
+  });
+})
